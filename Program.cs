6a99e59d67b6a4ab384bfe9f6ba3680a871d5e4b
xswaze <140498996+xswaze@users.noreply.github.com>
2023-08-22 14:44:16 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,39 @@
-﻿
-        // Заданный массив целых чисел
-        int[] numbers = { 1, 3, 5, 7, 9, 11, 13 };
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите числа, разделяя их пробелами:");
+        string input = Console.ReadLine();
+        string[] numberStrings = input.Split(' ');
+
+        int[] numbers = new int[numberStrings.Length];
+        for (int i = 0; i < numberStrings.Length; i++)
+        {
+            if (int.TryParse(numberStrings[i], out int num))
+            {
+                numbers[i] = num;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода числа " + numberStrings[i]);
+                return;
+            }
+        }
 
         // Посчитать сумму нечётных элементов в массиве
-        int sumnechet = 0;
+        int sumOfOddNumbers = 0;
         foreach (int num in numbers)
         {
             if (num % 2 != 0)
             {
-                sumnechet += num;
+                sumOfOddNumbers += num;
             }
         }
 
         // Найти среднее значение
-        double srznach = 0;
+        double average = 0;
         if (numbers.Length > 0)
         {
             int sum = 0;
@@ -21,26 +41,30 @@
             {
                 sum += num;
             }
-            srznach = (double)sum / numbers.Length;
+            average = (double)sum / numbers.Length;
         }
 
         // Найти медиану
-        double mediana;
+        double median = 0;
         int length = numbers.Length;
-        Array.Sort(numbers);
-        if (length % 2 == 0)
+        if (length > 0)
         {
-            int middle1 = numbers[length / 2 - 1];
-            int middle2 = numbers[length / 2];
-            mediana = (middle1 + middle2) / 2.0;
-        }
-        else
-        {
-            mediana = numbers[length / 2];
+            Array.Sort(numbers);
+            if (length % 2 == 0)
+            {
+                int middle1 = numbers[length / 2 - 1];
+                int middle2 = numbers[length / 2];
+                median = (middle1 + middle2) / 2.0;
+            }
+            else
+            {
+                median = numbers[length / 2];
+            }
         }
 
         // Вывести полученные значения в консоль
-        Console.WriteLine("Сумма нечётных элементов в массиве: " + sumnechet);
-        Console.WriteLine("Среднее значение: " + srznach);
-        Console.WriteLine("Медиана: " + mediana);
-
+        Console.WriteLine("Сумма нечётных элементов в массиве: " + sumOfOddNumbers);
+        Console.WriteLine("Среднее значение: " + average);
+        Console.WriteLine("Медиана: " + median);
+    }
+}
